579f2ce90a17043dcc4133bf603e560b1f5cbcbb
lunaplush <lunaplus@mail.ru>
2016-09-06 17:01:53 +0300

    cats 2

--- cats.py
+++ cats.py
@@ -6,6 +6,19 @@ Created on Tue Sep  6 16:15:38 2016
 """
 
 import numpy as np
+import re 
 
-x= np.array((1,2))
+f = open("sentences.txt","r")
 
+sentences = []
+words = []
+
+
+i = 0
+for line in f:    
+    sentences.append(line.lower())
+    words.append( re.split('[^a-z]', sentences[i]))
+    for word in words[i]:
+        if word ==0
+    i = i + 1
+f.close()
